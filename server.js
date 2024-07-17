32bd68e2727b8b42835fa72c2eaac964245f9c92
Ihab Arnus <ihabarnus@gmail.com>
2024-07-17 14:25:09 +0300

    serve static files

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const connectDB = require("./config/db");
 
 const app = express();
@@ -9,7 +10,7 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/api/about", (req, res) => {
   res.json({ msg: "welcome to contact keeper API" });
 });
 
@@ -18,6 +19,13 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 
+if (process.env.NOTE_ENV === "production") {
+  // set a static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
